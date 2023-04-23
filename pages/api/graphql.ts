600552a5abcb0007c9196ccb1dccc28f9a64f62c
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2023-04-23 12:58:08 +0000

    add graphql server

---
+++ pages/api/graphql.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { gql } from "graphql-tag";
+import { NextRequest } from 'next/server';
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+export default startServerAndCreateNextHandler<NextRequest>(server);
