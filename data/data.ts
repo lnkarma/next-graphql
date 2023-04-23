2750de7fe1c49bb9d7afecae92a39015235c2243
lnkarma <ilokenathkarmakar@gmail.com>
2023-04-23 21:05:49 +0530

    post resource

---
+++ data/data.ts
@@ -0,0 +1,2 @@
+export type Post = { id: string; title: string; likes: number };
+export let posts: Post[] = [];
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,17 +1,49 @@
+import { Post, posts } from "@/data/data";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { gql } from "graphql-tag";
-import { NextRequest } from 'next/server';
+import { NextRequest } from "next/server";
 
 const resolvers = {
   Query: {
     hello: () => "world",
+    posts: () => posts,
+  },
+
+  Mutation: {
+    addPost: (_: any, { title }: { title: string }) => {
+      const newPost: Post = {
+        id: Math.random().toString(),
+        title,
+        likes: 0,
+      };
+      posts.push(newPost);
+      return newPost;
+    },
   },
 };
 
 const typeDefs = gql`
+  type Post {
+    id: String
+    title: String
+    likes: Int
+  }
+
   type Query {
     hello: String
+    posts: [Post]
+  }
+
+  type Mutation {
+    addPost(title: String!): Post
+  }
+
+  mutation AddPost($title: String!) {
+    addPost(title: $title) {
+      id
+      title
+    }
   }
 `;
 
